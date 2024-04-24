eabdfb98f470f531423f8e706654a43f692a11c0
Yuri Cruger <aoddrewiid@gmail.com>
2024-04-24 10:54:39 -0300

    first commit: initial estructure and implementing database

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('HTTP Server Running!'))
